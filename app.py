d884a31ae550b3e1423995892fa7b77087d17a8d
Sergey Malych <sergey.malych@gmail.com>
2016-05-28 22:14:33 +0300

    remove hardcoded ip address

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ gic = pygeoip.GeoIP(r'./GeoLiteCity.dat')
 @ip.route("/")
 def get_ip():
     try:
-        geo = gic.record_by_addr('10.16.239.105')
+        geo = gic.record_by_addr(request.remote_addr)
         if not geo:
             raise
     except:
