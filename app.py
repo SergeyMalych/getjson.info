b81d9c5606f59341ddd7be6b49f126457fb40514
Sergey Malych <sergey.malych@gmail.com>
2016-05-28 18:15:24 +0300

    heroku configuration

--- app.py
+++ app.py
@@ -103,4 +103,5 @@ def index():
 ###############################################################################
 
 if __name__ == "__main__":
-    app.run(use_debugger=False, use_reloader=True, threaded=True)
+    port = int(os.environ.get('PORT',5000))
+    app.run(host='0.0.0.0', port=port, use_debugger=False, use_reloader=True, threaded=True)
