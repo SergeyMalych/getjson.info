c8ecd2eb51f10c32dace7b89ab831a4f185806ad
Sergey Malych <sergey.malych@gmail.com>
2016-05-28 19:42:49 +0300

    add currencies and languages

--- README.md
+++ README.md
@@ -6,6 +6,15 @@
 * `/ip/google.com` - get geodata about specified URI
 
 
-###Country codes
+###Countries (ISO 3166)
 * `/countries` - get data about all countries
-* `/countries/de` - get data about specified country by its alpha2 code(2 letter)<
+* `/countries/de` - get data about specified country by identifier
+
+
+###Currencies (ISO 4217)
+* `/currencies` - get data about all currencies
+* `/currencies/eur` - get data about specified currency by identifyer
+
+###Languages (ISO 639)
+* `/languages` - get data about all languages
+*/languages/he` - get data about specified language by identifyer
--- app.py
+++ app.py
@@ -9,7 +9,10 @@ gic = pygeoip.GeoIP(r'./GeoLiteCity.dat')
 
 @ip.route("/")
 def get_ip():
-    geo = gic.record_by_addr(request.remote_addr)
+    try:
+        geo = gic.record_by_addr(request.remote_addr)
+    except:
+        geo = 'geolocation could not be found'
     return jsonify({'ip': request.remote_addr,
         'geo': geo}), 200
     #for proxy:
@@ -52,12 +55,22 @@ def get_countries():
 
         result[country.name] = country_dict_temp
 
-    return jsonify(result)
+    return jsonify(result), 200
 
 @countries.route('/<country>')
-def get_country_by_alpha2(country):
+def get_country(country):
     try:
-        c = pycountry.countries.get(alpha2=str(country).upper())
+        try:
+            c = pycountry.countries.get(alpha2=str(country).upper())
+        except:
+            try:
+                c = pycountry.countries.get(alpha3=str(country).upper())
+            except:
+                try:
+                    c = pycountry.countries.get(numeric=str(country).upper())
+                except:
+                    c = pycountry.countries.get(name=str(country).upper())
+                    
         result = {}
         result['name'] = c.name
         result['numeric'] = c.numeric
@@ -68,9 +81,132 @@ def get_country_by_alpha2(country):
         except:
             pass
 
-        return jsonify(result)
+        return jsonify(result), 200
     except:
         return jsonify({'error': 'country code could not be found'})
+
+## currencies Blueprint ########################################################
+
+currencies = Blueprint('currencies', __name__)
+
+@currencies.route('/')
+def get_currencies():
+    result = {}
+    for currency in pycountry.currencies:
+        currency_dict_temp = {}
+        currency_dict_temp['name'] = currency.name
+        currency_dict_temp['letter'] = currency.letter
+        try:
+            currency_dict_temp['numeric'] = currency.numeric
+        except:
+            pass
+
+        result[currency.name] = currency_dict_temp
+
+    return jsonify(result), 200
+
+@currencies.route('/<letter>')
+def get_currency(letter):
+    try:
+        try:
+            c = pycountry.currencies.get(letter=str(letter).upper())
+        except:
+            try:
+                c = pycountry.currencies.get(numeric=str(letter).upper())
+            except:
+                c = pycountry.currencies.get(name=str(letter).upper())
+
+        result = {}
+        result['name'] = c.name
+        result['numeric'] = c.numeric
+        result['letter'] = c.letter
+
+        return jsonify(result), 200
+    except:
+        return jsonify({'error': 'currency could not be found'})
+
+
+
+## languages Blueprint ########################################################
+
+languages = Blueprint('languages', __name__)
+
+@languages.route('/')
+def get_languages():
+    result = {}
+    for language in pycountry.languages:
+        language_dict_temp = {}
+
+        try:
+            language_dict_temp['name'] = language.name
+        except:
+            pass
+        try:
+            language_dict_temp['inverted_name'] = language.inverted_name
+        except:
+            pass
+        try:
+            language_dict_temp['common_name'] = language.common_name
+        except:
+            pass
+        try:
+            language_dict_temp['iso639_1_code'] = language.iso639_1_code
+        except:
+            pass
+        try:
+            language_dict_temp['iso639_2T_code'] = language.iso639_2T_code
+        except:
+            pass
+        try:
+            language_dict_temp['iso639_3_code'] = language.iso639_3_code
+        except:
+            pass
+
+
+        result[language.name] = language_dict_temp
+
+    return jsonify(result), 200
+
+@languages.route('/<code>')
+def get_language(code):
+    try:
+        try:
+            language = pycountry.languages.get(iso639_1_code=str(code).lower())
+        except:
+            try:
+                language = pycountry.languages.get(iso639_2T_code=str(code).lower())
+            except:
+                language = pycountry.languages.get(iso639_3_code=str(code).lower())
+
+
+        result = {}
+
+        try:
+            result['name'] = language.name
+        except:
+            pass
+        try:
+            result['common_name'] = language.common_name
+        except:
+            pass
+        try:
+            result['iso639_1_code'] = language.iso639_1_code
+        except:
+            pass
+        try:
+            result['iso639_2T_code'] = language.iso639_2T_code
+        except:
+            pass
+        try:
+            result['iso639_3_code'] = language.iso639_3_code
+        except:
+            pass
+
+        return jsonify(result), 200
+    except:
+        return jsonify({'error': 'language could not be found'})
+
+
 
 ## app ########################################################################
 
@@ -78,6 +214,8 @@ app = Flask(__name__)
 app.config["DEBUG"] = True
 app.register_blueprint(ip, url_prefix="/ip")
 app.register_blueprint(countries, url_prefix="/countries")
+app.register_blueprint(currencies, url_prefix="/currencies")
+app.register_blueprint(languages, url_prefix="/languages")
 
 
 @app.route("/")
@@ -91,10 +229,22 @@ def index():
                     <li><a href="/ip/google.com"><b>/ip/google.com</b><br>get geodata about specified URI</a></li>
                 </ul>
             </li>
-            <li><h4>Country codes</h4>
+            <li><h4>Country codes (ISO 3166)</h4>
                 <ul>
-                    <li><a href="/countries"><b>/countries</b><br> get data about all countries</a></li>
-                    <li><a href="/countries/de"><b>/countries/de</b><br> get data about specified country by its alpha2 code(2 letter)</a></li>
+                    <li><a href="/countries"><b>/countries</b><br>get data about all countries</a></li>
+                    <li><a href="/countries/de"><b>/countries/de</b><br>get data about specified country</a></li>
+                </ul>
+            </li>
+            <li><h4>Currencies (ISO 4217)</h4>
+                <ul>
+                    <li><a href="/currencies"><b>/currencies</b><br>get data about all currencies</a></li>
+                    <li><a href="/currencies/eur"><b>/currencies/eur</b><br>get data about specified currency by identifyer</a></li>
+                </ul>
+            </li>
+            <li><h4>Languages (ISO 639)</h4>
+                <ul>
+                    <li><a href="/languages"><b>/languages</b><br>get data about all languages</a></li>
+                    <li><a href="/languages/heb"><b>/languages/heb</b><br>get data about specified language by identifyer</a></li>
                 </ul>
             </li>
             </ul>
